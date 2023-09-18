934aaf552bc51af971f6b40f9637bdc48f49bff5
c-kiplimo <kiplimocollins855@gmail.com>
2023-09-18 12:05:03 +0300

    learnrows,columns,box sizing

--- app/src/main/java/com/collicode/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/collicode/composecourseyt/MainActivity.kt
@@ -3,44 +3,29 @@ package com.collicode.composecourseyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.collicode.composecourseyt.ui.theme.ComposeCourseYTTheme
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeCourseYTTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+         Column( modifier=Modifier
+             .fillMaxSize(0.5f)
+             .background(Color.LightGray ) ,horizontalAlignment = Alignment.CenterHorizontally
+              , verticalArrangement = Arrangement.SpaceAround) {
+             Text(text = "Hello")
+             Text(text = "World")
+             Text(text = "Hello")
+         }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeCourseYTTheme {
-        Greeting("Android")
-    }
-}
