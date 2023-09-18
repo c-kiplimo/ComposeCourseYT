691cbd03191abcf5b1d7a5752459f5f148e1714a
c-kiplimo <kiplimocollins855@gmail.com>
2023-09-18 13:07:48 +0300

    Day1:learnrows,columns,box sizing

--- app/src/main/java/com/collicode/composecourseyt/Day1.kt
+++ app/src/main/java/com/collicode/composecourseyt/Day1.kt
@@ -6,19 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
-class MainActivity : ComponentActivity() {
+class Day1  : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Column( modifier= Modifier
+                .fillMaxSize(0.5f)
+                .background(Color.LightGray ) ,horizontalAlignment = Alignment.CenterHorizontally
+                , verticalArrangement = Arrangement.SpaceAround) {
+                Text(text = "Hello")
+                Text(text = "World")
+                Text(text = "Hello")
+            }
         }
     }
 }
-
